a99d39ca02c9faf937e9a7e89c520e9de1317e16
pkritt <krittapat37819@gmail.com>
2024-12-29 16:27:10 +0700

    draw fin

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+const int N = 30;
+const int M = 70;
+
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
+
+int main()
+{
+    bool image[N][M] = {};
+    int s,x,y;
+    do{
+        showImage(image);
+        cout << "Input your brush size and location: ";
+        cin >> s >> x >> y;
+        updateImage(image,s,x,y);
+    }while(s != 0 || x != 0 || y != 0);
+    return 0;
+}
+
+
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<M; j++){
+            if(sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1){
+                image[i][j] = true;
+            }
+        }
+    }
+}
+void showImage(const bool image[][M]) {
+    cout << "------------------------------------------------------------------------"<<endl;
+    for (int i = 0; i < N; i++) {
+        cout << "|";
+        for (int j = 0; j < M; j++) {
+            cout << (image[i][j] ? "*" : " ");
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << "------------------------------------------------------------------------"<<endl;
+}
