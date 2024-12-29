9ab5abbc929dc042f80fd94936b36c87fb8e405f
pkritt <krittapat37819@gmail.com>
2024-12-29 16:08:07 +0700

    stat

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
+void stat(const double[],int,double[]);
+
+int main(){
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
+    int N = sizeof(A)/sizeof(A[0]);
+    double B[6];
+    stat(A,N,B);
+    cout << fixed << setprecision(2);
+    cout << "Arithmetic Mean = " << B[0] << endl;
+    cout << "Standard Deviation = " << B[1] << endl;
+    cout << "Geometric Mean = " << B[2] << endl;
+    cout << "Harmonic Mean = " << B[3] << endl;
+    cout << "Max = " << B[4] << endl;
+    cout << "Min = " << B[5];
+    return 0;
+}
+
+void stat(const double A[],int N ,double B[]){
+   
+    for (int i = 0 ;i<N;i++){
+        B[0]= B[0]+A[i];
+    }
+    B[0]= B[0]/N;
+
+    for (int i = 0 ;i<N;i++){
+        B[1]= B[1]+pow(A[i]-B[0],2);
+        
+    }
+    B[1]= sqrt(B[1]/N-1);
+    B[2]= 1;
+    for (int i = 0 ;i<N;i++){
+        B[2]= B[2]*A[i];   
+    }
+    B[2] = pow(B[2],1.0/N);
+    for (int i = 0 ;i<N;i++){
+       B[3]= B[3]+(1/A[i]);
+    }
+    B[3]=N/B[3];
+
+    for (int i = 0 ;i<N;i++){
+       if (A[i]>B[4]) B[4]=A[i];
+       if (A[i]<B[5]) B[5]=A[i];
+    }
+
+} 
